7a3459a96ce1f6c18b8a86a5c39647d201af4066
nyshashina <nyshashina@gmail.com>
2023-02-04 19:04:59 +0300

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+void PrintMatrix(double[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j =0; j < arg.GetLength(1); j++)
+        {
+            Console.Write($"{arg[i,j]} \t ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void FillMatrix(double[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j =0; j < arg.GetLength(1); j++)
+        {
+            arg[i,j] = new Random().Next(-99,100);
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int numberM = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите количество столбцов");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+
+double[,]matrix = new double[numberM, numberN];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
